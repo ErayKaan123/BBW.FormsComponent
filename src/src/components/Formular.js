43b0160569cffff59650d3a8faed4159f758135a
Eray <eray.cevik@digitecgalaxus.ch>
2024-03-13 17:43:16 +0100

    e

--- src/src/components/Formular.js
+++ src/src/components/Formular.js
@@ -2,6 +2,8 @@ import './Formular.css';
 import '../App.css'
 import { useNavigate } from 'react-router-dom';
 import myjson from "./loginData/users.json";
+import Dialog from '../dialog/MessageBox';
+import MessageBox from '../dialog/MessageBox';
 
 function Formular(props) {
   const navigate = useNavigate();
@@ -15,28 +17,30 @@ function Formular(props) {
   }
 
   return (   
-    <div className="formular">
-      <form onSubmit={HandleSubmit} className="formular-container">
-        <div>
-          <h2 className="formular-title">SignUp</h2>
-          <p className="formular-description">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-          </p>
-          <div className="formular-items">
-            <div>
-              <p className="formular-label">Email</p>
-              <input inputMode='email' type='email' required className="input" id="emailField" placeholder="JohnDoe@gmail.com" />
-            </div>
-            
-            <div>
-              <p className="formular-label">Passwort</p>
-                <input type='password' inputMode='email' className="input" id="passwordField" placeholder="Example123" />
+    <div>
+      <div className="formular">
+        <form onSubmit={HandleSubmit} className="formular-container">
+          <div>
+            <h2 className="formular-title">SignUp</h2>
+            <p className="formular-description">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+            </p>
+            <div className="formular-items">
+              <div>
+                <p className="formular-label">Email</p>
+                <input inputMode='email' type='email' required className="input" id="emailField" placeholder="JohnDoe@gmail.com" />
               </div>
-            <button className="app-button app-button-primary" id="loginButton" type='submit'>Login</button>
+              
+              <div>
+                <p className="formular-label">Passwort</p>
+                  <input type='password' inputMode='email' className="input" id="passwordField" placeholder="Example123" />
+                </div>
+              <button className="app-button app-button-primary" id="loginButton" type='submit'>Login</button>
 
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+    </div>
     </div>
   )
 }
